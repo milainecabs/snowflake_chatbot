3850cde39f362f4ee22e5371cecbeaf43ec85710
Milaine <milainecabrelle@gmail.com>
2026-02-12 21:42:35 +0100

    Version ajustée

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,31 @@ import uuid
 # -------------------------------------------------
 # INIT
 # -------------------------------------------------
+# -------------------------------------------------
 
-session = get_active_session()
+
+from snowflake.snowpark import Session
+import os
+from dotenv import load_dotenv
+
+def create_session():
+    load_dotenv()
+
+    connection_parameters = {
+        "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+        "user": os.getenv("SNOWFLAKE_USER"),
+        "password": os.getenv("SNOWFLAKE_PASSWORD"),
+        "role": os.getenv("SNOWFLAKE_ROLE"),
+        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+        "database": os.getenv("SNOWFLAKE_DATABASE"),
+        "schema": os.getenv("SNOWFLAKE_SCHEMA"),
+    }
+
+    return Session.builder.configs(connection_parameters).create()
+
+session = create_session()
+
+#session = get_active_session()
 
 TABLE_NAME = "DB_LAB.CHAT_APP.CONVERSATIONS"
 
